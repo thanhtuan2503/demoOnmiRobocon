d1315d56882c24539d5601e746a60297a067a735
phithanhdanh <74167473+phithanhdanh@users.noreply.github.com>
2020-11-21 16:55:47 +0700

    Them chu thich huong dan ket noi DUALSHOCK 4

--- omniControl.ino
+++ omniControl.ino
@@ -25,10 +25,10 @@ BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
 /* You can create the instance of the PS4BT class in two ways */
 // This will start an inquiry and then pair with the PS4 controller - you only have to do this once
 // You will need to hold down the PS and Share button at the same time, the PS4 controller will then start to blink rapidly indicating that it is in pairing mode
-PS4BT PS4(&Btd, PAIR);
+PS4BT PS4(&Btd, PAIR); //Ket noi lan dau bang cach nhan nut share và nut PS.
 
 // After that you can simply create the instance like so and then press the PS button on the device
-//PS4BT PS4(&Btd);
+//PS4BT PS4(&Btd); //Thay lenh phia tren bang lenh nay sau khi ket noi thanh cong, nap lai code, lan sau chi can nhan nut PS de ket noi.
 
 bool printAngle, printTouch;
 uint8_t oldL2Value, oldR2Value;
